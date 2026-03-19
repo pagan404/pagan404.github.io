8725e3b66a76a0c9b6f11a65d28a0a742807268f
Pagan404 <76445757+pagan404@users.noreply.github.com>
2026-03-19 12:47:02 +0100

    Changed the text in the footer

--- react-app/src/components/layout/Footer.jsx
+++ react-app/src/components/layout/Footer.jsx
@@ -2,7 +2,8 @@ function Footer() {
   return (
     <footer>
       <p>
-        <a href="https://github.com/pagan404">GitHub</a>
+        This website is made to store my programming projects and an occasional
+        blog. If you'd like to contact me - please don't!
       </p>
     </footer>
   );
